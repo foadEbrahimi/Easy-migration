039955faf5d6d8a10a48c078860db728af95d301
Foad <fwad40901@gmail.com>
2025-09-23 01:01:43 +0330

    create Navbar

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import logo from '../../../public/images/logo.svg';
+import phone from '../../../public/images/layout/phone.svg';
+import SideMenu from './SideMenu';
+
+const links = [
+  { id: 1, label: 'خانه', href: '/' },
+  { id: 2, label: 'خدمات', href: '/' },
+  { id: 3, label: 'تماس با ما', href: '/' },
+  { id: 4, label: 'درباره ما', href: '/' },
+  { id: 5, label: 'وبلاگ', href: '/' },
+];
+
+export default function Navbar() {
+  return (
+    <nav className='max-w-7xl mx-auto flex items-center justify-between p-5 lg:px-10 xl:px-16 2xl:px-0 relative'>
+      {/* لوگو */}
+      <div className='flex items-center gap-1'>
+        <Image src={logo} alt='logo' className='w-12 lg:w-16' />
+        <span className='lg:text-2xl font-medium'>
+          <span className='text-blue-500'>مهاجرت</span> آسان
+        </span>
+      </div>
+
+      {/* لینک‌ها */}
+      <ul className='hidden md:flex items-center gap-11'>
+        {links.map(link => (
+          <li key={link.id} className='listItem lg:text-xl'>
+            {link.label}
+          </li>
+        ))}
+      </ul>
+
+      {/* تماس */}
+      <div className='hidden md:flex items-center gap-2'>
+        <div className='bg-blue-500 rounded-full w-9 h-9 xl:w-11 xl:h-11 flex items-center justify-center'>
+          <Image src={phone} alt='phone' />
+        </div>
+        <span className='font-medium text-sm'>
+          برای مشاوره تماس بگیرید
+          <br />
+          021123456789
+        </span>
+      </div>
+
+      {/* سایدبار موبایل */}
+      <SideMenu />
+    </nav>
+  );
+}
---
+++ src/components/layout/SideMenu.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { useState } from 'react';
+import useClickOutSide from '@/hooks/useClickOutSide';
+
+import logo from '../../../public/images/logo.svg';
+import list from '../../../public/images/layout/list.svg';
+import Image from 'next/image';
+import phone from '../../../public/images/layout/phone.svg';
+import chevronLeft from '../../../public/images/layout/chevronLeft.svg';
+
+export default function SideMenu() {
+  const [showSide, setShowSide] = useState(false);
+  const close = () => setShowSide(false);
+  const { ref } = useClickOutSide(close);
+
+  return (
+    <div className='md:hidden'>
+      <button onClick={() => setShowSide(true)} className='cursor-pointer'>
+        <Image src={list} className='w-7 md:w-8 ' alt='close' />
+      </button>
+
+      {showSide && (
+        <div
+          className='fixed inset-0 bg-black/50 z-20 transition-opacity'
+          onClick={close}
+        ></div>
+      )}
+
+      <div
+        ref={ref}
+        className={`fixed top-0 right-0 h-full w-[80%] max-w-md bg-white z-30 transform transition-transform duration-500
+        ${showSide ? 'translate-x-0' : 'translate-x-full'}`}
+      >
+        <div className='p-4 flex items-center justify-between'>
+          <div className='flex items-center gap-2'>
+            <Image src={logo} alt='logo' className='w-12 lg:w-16' />
+            <span className='lg:text-2xl font-medium'>
+              <span className='text-blue-500'>مهاجرت</span> آسان
+            </span>
+          </div>
+        </div>
+
+        <div className='px-5 py-3 pt-10 flex flex-col h-[calc(100%-5rem)]'>
+          <ul className='space-y-10 text-lg'>
+            <li className='flex items-center text-xl font-semibold w-full sidebaeItem justify-between'>
+              صفحه اصلی <Image src={chevronLeft} alt='chevronLeft' />
+            </li>
+            <li className='flex items-center text-xl font-semibold w-full sidebaeItem justify-between'>
+              درباره ما <Image src={chevronLeft} alt='chevronLeft' />
+            </li>
+            <li className='flex items-center text-xl font-semibold w-full sidebaeItem justify-between'>
+              خدمات <Image src={chevronLeft} alt='chevronLeft' />
+            </li>
+            <li className='flex items-center text-xl font-semibold w-full sidebaeItem justify-between'>
+              تماس با ما <Image src={chevronLeft} alt='chevronLeft' />
+            </li>
+          </ul>
+
+          <div className='flex items-center gap-2 mt-auto'>
+            <div className='bg-blue-500 rounded-full w-9 h-9 xl:w-11 xl:h-11 flex items-center justify-center'>
+              <Image src={phone} alt='phone' />
+            </div>
+            <span className='font-medium text-sm'>
+              برای مشاوره تماس بگیرید
+              <br />
+              021123456789
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
