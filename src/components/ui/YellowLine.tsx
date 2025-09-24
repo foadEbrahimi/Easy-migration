31a49d00f703d18eb3565aa468650f7ee7b428c8
Foad <fwad40901@gmail.com>
2025-09-24 11:45:11 +0330

    create YellowLine component

---
+++ src/components/ui/YellowLine.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function YellowLine() {
+  return (
+    <div className='flex items-center gap-2'>
+      <span className='bg-[#f6cb32] w-28 h-1 rounded-full' />
+      <span className='bg-[#f6cb32] w-1.5 h-1 rounded-full' />
+    </div>
+  );
+}
